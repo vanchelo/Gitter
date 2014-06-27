2f7b8f0f0ef606bfca50c1f1ac8b978165ce4b8f
vanchelo <npobolka@ya.ru>
2014-06-27 03:35:03 +0300

    Fix indentation

--- src/Laravelrus/Gitter/migrations/2014_06_26_233727_create_gitter_users.php
+++ src/Laravelrus/Gitter/migrations/2014_06_26_233727_create_gitter_users.php
@@ -5,34 +5,34 @@ use Illuminate\Database\Schema\Blueprint;
 
 class CreateGitterUsers extends Migration {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		Schema::create('gitter_users', function(Blueprint $table)
-		{
-			$table->string('id', 24);
-    			$table->primary('id');
-			$table->string('username', 255);
-			$table->string('displayName', 255);
-			$table->string('url', 255);
-			$table->string('avatarUrlSmall', 255);
-			$table->string('avatarUrlMedium', 255);
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gitter_users', function(Blueprint $table)
+        {
+            $table->string('id', 24);
+            $table->primary('id');
+            $table->string('username', 255);
+            $table->string('displayName', 255);
+            $table->string('url', 255);
+            $table->string('avatarUrlSmall', 255);
+            $table->string('avatarUrlMedium', 255);
+        });
+    }
 
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		Schema::drop('gitter_users');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('gitter_users');
+    }
 
 }
